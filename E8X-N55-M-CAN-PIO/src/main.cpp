a8d141c1c0d80ad0c332b90cce4f6eb2063f6b31
lomelo-x <aalomelo@gmail.com>
2025-06-07 11:47:34 -0700

    Refactor PlatformIO configuration: remove Unity dependency and clean up main.cpp by eliminating unused CAN bus definitions

--- E8X-N55-M-CAN-PIO/src/main.cpp
+++ E8X-N55-M-CAN-PIO/src/main.cpp
@@ -1,10 +1,5 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
-#include <imxrt_flexcan.h>
-
-// CAN bus definitions
-#define CAN1 CAN1
-#define CAN2 CAN2
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> KCAN; // K-CAN (100kbps)
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> PTCAN; // PT-CAN (500kbps)
