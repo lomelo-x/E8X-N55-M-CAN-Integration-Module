784ec72acc354e53f9408145087482cb5ca012b2
lomelo-x <aalomelo@gmail.com>
2025-06-11 15:37:20 -0700

    Refactor gauge sweep functionality to include rapid blink flag and clean up unused code

--- src/gauge_sweep.cpp
+++ src/gauge_sweep.cpp
@@ -1,8 +1,10 @@
 #include "gauge_sweep.h"
-#include "can_core.h"
+#include <Arduino.h>
 
-
-
+// Extern CAN objects and buffers
+extern FlexCAN_T4<CAN_DEV_TABLE::CAN1, RX_SIZE_256, TX_SIZE_16> KCAN;
+extern CAN_message_t k_msg;
+extern bool rapid_blink;
 
 void initializeGaugeMessages() {
     // Initialize message structures for gauge sweep
@@ -11,16 +13,10 @@ void initializeGaugeMessages() {
     k_msg.flags.remote = 0;
     k_msg.flags.overrun = 0;
     k_msg.flags.reserved = 0;
-
-    pt_msg.len = 8;
-    pt_msg.flags.extended = 0;
-    pt_msg.flags.remote = 0;
-    pt_msg.flags.overrun = 0;
-    pt_msg.flags.reserved = 0;
 }
 
 void performGaugeSweep() {
-    // Perform gauge sweep animation
+    rapid_blink = true;
     for (uint8_t i = 0; i <= 100; i++) {
         // Update speedometer (0x1A6)
         k_msg.id = 0x1A6;
@@ -39,4 +35,5 @@ void performGaugeSweep() {
 
         delay(50); // Adjust speed of sweep
     }
+    rapid_blink = false;
 } 
