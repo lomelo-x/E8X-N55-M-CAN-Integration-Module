cf09bfb0dab49e81855fd9696ac33759996a9d27
lomelo-x <aalomelo@gmail.com>
2025-06-11 15:37:27 -0700

    Add extern declaration for rapid_blink flag and function prototypes in gauge_sweep.h

--- src/gauge_sweep.h
+++ src/gauge_sweep.h
@@ -1,1 +1,13 @@
+#ifndef GAUGE_SWEEP_H
+#define GAUGE_SWEEP_H
 
+#include <FlexCAN_T4.h>
+
+// Extern for rapid_blink flag
+extern bool rapid_blink;
+
+// Function declarations
+void initializeGaugeMessages();
+void performGaugeSweep();
+
+#endif // GAUGE_SWEEP_H
